2bce62a09dcf8a5790a573587e7f96768aa83ca3
jamalamar <jamal_ab22@hotmail.com>
2020-03-02 18:03:02 -0600

    ready to start

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+
+/*Components for Spanish and English*/
+import Inicio from './Views/Inicio'
+import Home from './Views/Home'
+
+import Nosotros from './Views/Nosotros'
+import AboutUs from './Views/AboutUs'
+
+import Eventos from './Views/Eventos'
+import Events from './Views/Events'
+
+import Espacios from './Views/Espacios'
+import Spaces from './Views/Spaces'
+
+import Contacto from './Views/Espacios'
+import Contact from './Views/Spaces'
 
 function App() {
+
+  /*Hook to change Language*/
+  const [lan, setLang] = useState('es')
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Switch>
+          <Route path='/Inicio' exact component={Inicio} />
+          <Route path='/en/Home' exact component={Home} />
+
+          <Route path='/Nosotros' component={Nosotros} />
+          <Route path='/en/AboutUs' component={AboutUs} />
+          
+          <Route path='/Eventos' component={Eventos} />
+          <Route path='/en/Events' component={Events} />
+          
+          <Route path='/Espacios' component={Espacios} />
+          <Route path='/en/Spaces' component={Spaces} />
+
+          <Route path='/Contacto' component={Contacto} />
+          <Route path='/en/Contact' component={Contact} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
