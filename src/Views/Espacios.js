73893f59238d3d7e1b47b5e61c0a65913f26fe41
jamalamar <jamal_ab22@hotmail.com>
2020-03-06 13:43:10 -0600

    espacios ready

--- src/Views/Espacios.js
+++ src/Views/Espacios.js
@@ -4,7 +4,7 @@ import { MDBContainer, MDBRow, MDBCol, MDBCard, MDBCardBody, MDBMask, MDBIcon, M
 const Espacios = () => {
   return (
   	<div className="pt-5">
-    <MDBCard className="my-5 px-5 pb-5">
+    <div className="w-responsive mx-auto pt-5">
       <MDBCardBody>
         <MDBRow>
           <MDBCol md="12">
@@ -14,40 +14,17 @@ const Espacios = () => {
                   src="https://mdbootstrap.com/img/Photos/Slides/img%20(142).jpg"
                   alt=""
                   className="img-fluid"
+                  class="card-img"
                 />
                 <MDBMask overlay="white-slight" className="waves-light" />
               </MDBView>
-              <MDBCardBody cascade className="text-center">
+            </MDBCard>
+
+            <MDBCard className='mx-5' style={{'marginTop': '-3rem'}}>
+              <MDBCardBody className="text-center">
                 <h2 className="font-weight-bold">
-                  <a href="#!">Title of the news</a>
+                  Espacio 1
                 </h2>
-                <p>
-                  Written by
-                  <a href="#!">
-                    <strong>Abby Madison</strong>
-                  </a>
-                  , 26/08/2018
-                </p>
-                <MDBBtn className="btn-fb waves-light">
-                  <MDBIcon fab icon="facebook-f" className="pr-2" />
-                  Facebook
-                </MDBBtn>
-                <span className="counter">46</span>
-                <MDBBtn className="btn-tw waves-light">
-                  <MDBIcon fab icon="twitter" className="pr-2" />
-                  Twitter
-                </MDBBtn>
-                <span className="counter">22</span>
-                <MDBBtn className="btn-gplus waves-light">
-                  <MDBIcon fab icon="google-plus-g" className="pr-2" />
-                  Google
-                </MDBBtn>
-                <span className="counter">31</span>
-                <MDBBtn color="default" className="waves-light">
-                  <MDBIcon icon="comments" className="pr-2" />
-                  Comments
-                </MDBBtn>
-                <span className="counter">18</span>
               </MDBCardBody>
             </MDBCard>
             <MDBContainer className="mt-5">
@@ -81,40 +58,17 @@ const Espacios = () => {
                   src="https://mdbootstrap.com/img/Photos/Slides/img%20(146).jpg"
                   alt=""
                   className="img-fluid"
+                  class="card-img"
                 />
                 <MDBMask overlay="white-slight" className="waves-light" />
               </MDBView>
-              <MDBCardBody cascade className="text-center">
+            </MDBCard>
+
+            <MDBCard className='mx-5' style={{'marginTop': '-3rem'}}>
+              <MDBCardBody className="text-center">
                 <h2 className="font-weight-bold">
-                  <a href="#!">Title of the news</a>
+                  Espacio 2
                 </h2>
-                <p>
-                  Written by
-                  <a href="#!">
-                    <strong>Abby Madison</strong>
-                  </a>
-                  , 21/08/2018
-                </p>
-                <MDBBtn className="btn-fb waves-light">
-                  <MDBIcon fab icon="facebook-f" className="pr-2" />
-                  Facebook
-                </MDBBtn>
-                <span className="counter">87</span>
-                <MDBBtn className="btn-tw waves-light">
-                  <MDBIcon fab icon="twitter" className="pr-2" />
-                  Twitter
-                </MDBBtn>
-                <span className="counter">73</span>
-                <MDBBtn className="btn-gplus waves-light">
-                  <MDBIcon fab icon="google-plus-g" className="pr-2" />
-                  Google
-                </MDBBtn>
-                <span className="counter">91</span>
-                <MDBBtn color="default" className="waves-light">
-                  <MDBIcon icon="comments" className="pr-2" />
-                  Comments
-                </MDBBtn>
-                <span className="counter">67</span>
               </MDBCardBody>
             </MDBCard>
             <MDBContainer className="mt-5">
@@ -139,8 +93,97 @@ const Espacios = () => {
             </MDBContainer>
           </MDBCol>
         </MDBRow>
+        <hr className="mb-5 mt-4" />
+        <MDBRow>
+          <MDBCol md="12">
+            <MDBCard reverse>
+              <MDBView hover cascade waves>
+                <img
+                  src="https://mdbootstrap.com/img/Photos/Slides/img%20(148).jpg"
+                  alt=""
+                  className="img-fluid"
+                  class="card-img"
+                />
+                <MDBMask overlay="white-slight" className="waves-light" />
+              </MDBView>
+            </MDBCard>
+
+            <MDBCard className='mx-5' style={{'marginTop': '-3rem'}}>
+              <MDBCardBody className="text-center">
+                <h2 className="font-weight-bold">
+                  Espacio 3
+                </h2>
+              </MDBCardBody>
+            </MDBCard>
+            <MDBContainer className="mt-5">
+              <p>
+                At vero eos et accusamus et iusto odio dignissimos ducimus qui
+                praesentium voluptatum deleniti atque corrupti quos dolores et
+                quas molestias excepturi sint occaecati cupiditate non
+                provident, similique sunt in culpa nemo enim ipsam voluptatem
+                quia voluptas sit qui officia deserunt mollitia animi, id est
+                laborum et dolorum fuga quidem rerum facilis est distinctio.
+              </p>
+              <p>
+                Nam libero tempore, cum soluta nobis est eligendi optio cumque
+                nihil impedit quo minus id quod maxime placeat facere
+                possimus, omnis voluptas assumenda est, omnis dolor
+                repellendus. Quis autem vel eum iure reprehenderit qui in ea
+                voluptate velit esse quam nihil molestiae consequatur.
+                Temporibus autem quibusdam et aut officiis debitis aut rerum
+                necessitatibus saepe eveniet ut et voluptates repudiandae sint
+                et molestiae non recusandae itaque earum rerum.
+              </p>
+            </MDBContainer>
+          </MDBCol>
+        </MDBRow>
+		<hr className="mb-5 mt-4" />
+        <MDBRow>
+          <MDBCol md="12">
+            <MDBCard reverse>
+              <MDBView hover cascade waves>
+                <img
+                  src="https://mdbootstrap.com/img/Photos/Slides/img%20(144).jpg"
+                  alt=""
+                  className="img-fluid"
+                  class="card-img"
+                />
+                <MDBMask overlay="white-slight" className="waves-light" />
+              </MDBView>
+            </MDBCard>
+
+            <MDBCard className='mx-5' style={{'marginTop': '-3rem'}}>
+              <MDBCardBody className="text-center">
+                <h2 className="font-weight-bold">
+                  Espacio 4
+                </h2>
+              </MDBCardBody>
+            </MDBCard>
+            <MDBContainer className="mt-5">
+              <p>
+                At vero eos et accusamus et iusto odio dignissimos ducimus qui
+                praesentium voluptatum deleniti atque corrupti quos dolores et
+                quas molestias excepturi sint occaecati cupiditate non
+                provident, similique sunt in culpa nemo enim ipsam voluptatem
+                quia voluptas sit qui officia deserunt mollitia animi, id est
+                laborum et dolorum fuga quidem rerum facilis est distinctio.
+              </p>
+              <p>
+                Nam libero tempore, cum soluta nobis est eligendi optio cumque
+                nihil impedit quo minus id quod maxime placeat facere
+                possimus, omnis voluptas assumenda est, omnis dolor
+                repellendus. Quis autem vel eum iure reprehenderit qui in ea
+                voluptate velit esse quam nihil molestiae consequatur.
+                Temporibus autem quibusdam et aut officiis debitis aut rerum
+                necessitatibus saepe eveniet ut et voluptates repudiandae sint
+                et molestiae non recusandae itaque earum rerum.
+              </p>
+            </MDBContainer>
+          </MDBCol>
+        </MDBRow>
+        <hr className="mb-5 mt-4" />
       </MDBCardBody>
-    </MDBCard>
+    </div>
     </div>
   );
 }
